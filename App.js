72bb38b1775b23abb916837e5453330880affc1b
JigarRajput <rajputjigar455@gmail.com>
2023-02-19 18:32:58 +0530

    Pushing Home Page half done

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <Text style={styles.textColor}>Jai Ganesh</Text> */}
+      <View style={styles.firstContainer}>
+        <Text style={styles.findYourText}>Find Your</Text>
+        <Text style={styles.sideKickText}>SideKick</Text>
+        <View style={{ display: 'flex', flexDirection: 'row', marginTop: 30 }}>
+          <TextInput
+            style={styles.searchInput}
+            // onChangeText={onChangeNumber}
+            // value={number}
+            placeholder="Search area..." // keyboardType="numeric" 
+          />
+          <TouchableOpacity style={styles.searchBtn}>
+            <Text>Search</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+
+    </SafeAreaView>
   );
 }
 
@@ -15,6 +30,43 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 25 : 0
+    // justifyContent: 'center',
   },
+  firstContainer: {
+    display: 'flex',
+    width: '95%',
+    height: 200,
+    backgroundColor: '#FFFACC',
+    borderRadius: 11,
+    padding: 30
+    // marginTop: 20
+  },
+  findYourText: {
+    // fontWeight: 400,
+    fontSize: 16
+  },
+  sideKickText: {
+    fontSize: 40,
+    fontWeight: 'bold'
+  },
+  searchBtn: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 75,
+    height: 34,
+    backgroundColor: 'white',
+    borderRadius: 16
+  },
+  searchInput: {
+    width: 195,
+    height: 34,
+    borderRadius: 15,
+    backgroundColor: 'white',
+    paddingStart: 10,
+    marginEnd: 20
+  }
+
 });
